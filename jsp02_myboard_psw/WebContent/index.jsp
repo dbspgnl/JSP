46a442aeced8a7116dc542bbff58b883ba3f2e03
user2 <user2@user2-PC>
2020-01-03 21:46:27 +0900

    Add JSP02

---
+++ jsp02_myboard_psw/WebContent/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<% request.setCharacterEncoding("UTF-8"); %>
+<% response.setContentType("text/html; charset=UTF-8"); %>    
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<a href="./mylist.jsp">list</a><br/>
+	<a href="mylist.jsp">해당파일</a><br/>
+	<a href="/jsp02_myboard_psw/mylist.jsp">전체경로로 진입</a>
+	<%-- <jsp:forward page="./mylist.jsp"></jsp:forward> --%>
+	
+</body>
+</html>
---
+++ jsp02_myboard_psw/src/com/my/dao/MyDao.java
@@ -0,0 +1,165 @@
+package com.my.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.my.dto.MyDto;
+import static com.my.jdbc.JDBCTemplate.*;
+
+public class MyDao {
+
+	public List<MyDto> selectList(){
+		//준비
+		Connection con = getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		List<MyDto> list = new ArrayList<MyDto>();
+		String sql = " SELECT MYNO, MYNAME, MYTITLE, MYCONTENT, "
+				+ " MYDATE FROM MYBOARD ORDER BY MYNO DESC ";
+		//실행
+		try {
+			stmt = con.createStatement();
+			System.out.println("3 쿼리준비");
+			rs = stmt.executeQuery(sql);
+			System.out.println("4 실행리턴");
+			while(rs.next()) {
+				MyDto dto = new MyDto();
+				dto.setMyno(rs.getInt(1));
+				dto.setMyname(rs.getString(2));
+				dto.setMytitle(rs.getString(3));
+				dto.setMycontent(rs.getString(4));
+				dto.setMydate(rs.getDate(5));
+				
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			System.out.println("ERROR 3,4");
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+				System.out.println("5 종료");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("ERROR 5");
+			}
+		}
+		return list;
+	}
+	
+	public MyDto selectOne(int myno) {
+		Connection con = getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = " SELECT MYNO, MYNAME, MYTITLE, MYCONTENT, "
+				+ " MYDATE FROM MYBORAD WHERE MYNO = " + myno;
+		MyDto res = null;
+		
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				res = new MyDto(rs.getInt(1), rs.getString(2), 
+					rs.getString(3), rs.getString(4), rs.getDate(5));
+			}
+			System.out.println("3,4 실행 리턴");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("ERROR 3,4");
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+				System.out.println("5 db종료");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("EEROR 5");
+			}
+		}
+		return res;
+	}
+	
+	public int insert(MyDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		String sql = " INSERT INTO MYBOARD VALUES(MYSEQ.NEXTVAL,?,?,?,SYSDATE) ";
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, dto.getMyname());
+			pstm.setString(2, dto.getMytitle());
+			pstm.setString(3, dto.getMycontent());
+			
+			res = pstm.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstm.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
+	
+	public int update(MyDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		String sql = " UPDATE MYBOARD SET MYNAME=?, "
+				+ " MYTITLE=? MYCONTENT=? WHERE MYNO=? ";
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, dto.getMyname());
+			pstm.setString(2, dto.getMytitle());
+			pstm.setString(3, dto.getMycontent());
+			
+			res = pstm.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstm.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
+	
+	public int delete(int myno) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		String sql = " DELETE FROM MYBOARD WHERE =? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setInt(1, myno);
+			
+			res = pstm.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstm.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
+	
+}
